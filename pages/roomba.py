1a07721614ab97ac925407410d75bda1468a7b5b
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2023-07-16 13:37:55 +0200

    Update roomba.py

--- pages/roomba.py
+++ pages/roomba.py
@@ -74,12 +74,15 @@ with col4:
     st.write(":calendar:")
 with col5:
     message = get_the_right_room(commande)
-    if message is not None:
+    if message is not None and message != "Fichtre !":
         st.write(f"Date: {message[4]}")
         st.write(f"Salle(s): {message[0]}")
         st.write(f"Commence à: {message[1]}")
         st.write(message[2])
         st.write(message[3])
     else:
-        st.write("Aucun événement trouvé pour cette date.")
+        if message == "Fichtre !":
+            st.write(message)
+        else:
+            st.write("Aucun événement trouvé pour cette date.")
     
