b53a59e6d20b4157a90b0e39fe0b673cbd463b5a
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2023-07-17 02:25:32 +0200

    Update roomba.py

--- pages/roomba.py
+++ pages/roomba.py
@@ -74,7 +74,7 @@ col4, col5, col6 = st.columns(3)
 with col4:
     st.write(":calendar:")
 with col5:
-    if not commande:
+    if commande !="matin" or commande != "aprem" or commande !="demain":
         message = get_the_right_room(commande)
     if message != "Fichtre !":
         st.write(f"Date: {message[4]}")
